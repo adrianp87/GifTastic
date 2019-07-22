d655d0c344c452e706f3d0826b9f7cfbf5f77413
Adrian Perez <adrian.perez87@gmail.com>
2019-07-22 15:10:12 -0700

    pseudocode

---
+++ assets/js/app.js
@@ -0,0 +1,16 @@
+// Define variables ( const = topics)
+
+// create an array of strings with Cardi B Images lol and add to topics variable
+
+// connect API
+
+//create function to generate images
+
+//onclick, generate static gifs/images
+
+
+//once generated, if user clicks on image, the image should animate and stop animation on second click. 
+
+// create function to animate gifs
+
+//create function to stop animation
